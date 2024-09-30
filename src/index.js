ddabf29ab6e39e08882954dcd70bf463de4b7847
rekha0suthar <rekha0suthar@gmail.com>
2024-09-30 13:40:25 +0530

    issue fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { RecipeProvider } from './context/RecipleContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RecipeProvider>
-      <App />
-    </RecipeProvider>
+    <App />
   </React.StrictMode>
 );
